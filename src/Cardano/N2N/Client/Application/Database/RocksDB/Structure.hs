d74884533362368491e14c97678bde5a8a57781c
paolino <paolo.veronelli@gmail.com>
2026-01-15 15:12:21 +0000

    feat: add rollback primitive for rocks db backend

--- src/Cardano/N2N/Client/Application/Database/RocksDB/Structure.hs
+++ src/Cardano/N2N/Client/Application/Database/RocksDB/Structure.hs
@@ -1,9 +1,11 @@
 module Cardano.N2N.Client.Application.Database.RocksDB.Structure
     ( Structure (..)
-    , RollBackPoint (..)
+    , RollbackPoint (..)
     , RollbackPointKV
     , forwardTipApplyRocksDB
     , updateRollbackPoint
+    , rollbackTipApply
+    , RollbackResult (..)
     )
 where
 
@@ -14,27 +16,40 @@ import Cardano.N2N.Client.Application.Database.Interface
     ( Operation (..)
     )
 import Control.Lens ((:~:) (..))
-import Control.Monad (forM)
+import Control.Monad (forM, forM_, when)
+import Control.Monad.Trans (MonadIO (..), lift)
+import Data.Function (fix)
+import Data.List.SampleFibonacci (sampleAtFibonacciIntervals)
+import Database.KV.Cursor
+    ( Cursor
+    , Entry (..)
+    , lastEntry
+    , prevEntry
+    , seekKey
+    )
 import Database.KV.Transaction
     ( GCompare (..)
     , GEq (..)
     , GOrdering (..)
     , KV
     , Transaction
+    , delete
     , insert
+    , iterating
     , query
     )
-import Database.RocksDB
+import Database.RocksDB (BatchOp, ColumnFamily)
+import Ouroboros.Network.Point (WithOrigin)
 
 -- | Represents a rollback point in the database
-data RollBackPoint slot hash key value = RollBackPoint
+data RollbackPoint slot hash key value = RollbackPoint
     { rbpHash :: hash
     , rbpInverseOperations :: [Operation key value]
     }
 
 -- | Type alias for the RocksDB KV column storing rollback points
 type RollbackPointKV slot hash key value =
-    KV slot (RollBackPoint slot hash key value)
+    KV (WithOrigin slot) (RollbackPoint slot hash key value)
 
 -- | Structure of the RocksDB database used by this application
 data Structure slot hash key value x where
@@ -69,7 +84,7 @@ forwardTipApplyRocksDB
     -- ^ FromKV instance to convert from key to Key and value to hash
     -> Hashing hash
     -- ^ Way to compose hashes
-    -> slot
+    -> WithOrigin slot
     -- ^ slot at which operations happen
     -> hash
     -- ^ hash of the block at which operations happen
@@ -93,10 +108,65 @@ forwardTipApplyRocksDB fromKV hashing slot hash ops = do
 
 updateRollbackPoint
     :: Ord slot
-    => slot
+    => WithOrigin slot
     -> hash
     -> [Operation key value]
     -> Transaction IO ColumnFamily (Structure slot hash key value) BatchOp ()
-updateRollbackPoint slot hash ops =
+updateRollbackPoint slot rbpHash rbpInverseOperations =
     insert RollbackPoints slot
-        $ RollBackPoint{rbpHash = hash, rbpInverseOperations = ops}
+        $ RollbackPoint{rbpHash, rbpInverseOperations}
+
+sampleRollbackPoints
+    :: Cursor
+        (Transaction IO ColumnFamily (Structure slot hash key value) BatchOp)
+        (RollbackPointKV slot hash key value)
+        [WithOrigin slot]
+sampleRollbackPoints = do
+    fmap entryKey <$> sampleAtFibonacciIntervals prevEntry
+
+rollbackRollbackPoint
+    :: Ord key
+    => FromKV key value hash
+    -> Hashing hash
+    -> RollbackPoint slot hash key value
+    -> Transaction IO ColumnFamily (Structure slot hash key value) BatchOp ()
+rollbackRollbackPoint fromKV hashing RollbackPoint{rbpInverseOperations} =
+    forM_ rbpInverseOperations $ \case
+        Insert k v -> inserting fromKV hashing KVCol CSMTCol k v
+        Delete k -> deleting fromKV hashing KVCol CSMTCol k
+
+data RollbackResult slot
+    = RollbackSucceeded
+    | RollbackFailedButPossible [WithOrigin slot]
+    | RollbackImpossible
+
+rollbackTipApply
+    :: (Ord slot, Eq hash, Ord key)
+    => FromKV key value hash
+    -> Hashing hash
+    -> WithOrigin slot
+    -> hash
+    -> Transaction
+        IO
+        ColumnFamily
+        (Structure slot hash key value)
+        BatchOp
+        (RollbackResult slot)
+rollbackTipApply fromKV hashing slot hash = do
+    iterating RollbackPoints $ do
+        me <- seekKey slot
+        case me of
+            Nothing -> pure RollbackImpossible
+            Just (Entry foundSlot RollbackPoint{rbpHash = foundHash}) -> do
+                if foundSlot == slot && foundHash == hash
+                    then do
+                        ml <- lastEntry
+                        ($ ml) $ fix $ \go current -> case current of
+                            Nothing -> liftIO $ fail "rollbackTipApply: inconsistent rollback points"
+                            Just Entry{entryKey, entryValue} -> when (entryKey > slot) $ do
+                                lift $ do
+                                    rollbackRollbackPoint fromKV hashing entryValue
+                                    delete RollbackPoints entryKey
+                                prevEntry >>= go
+                        pure RollbackSucceeded
+                    else RollbackFailedButPossible <$> sampleRollbackPoints
