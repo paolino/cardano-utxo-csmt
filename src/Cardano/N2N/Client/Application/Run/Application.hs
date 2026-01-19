2ce1db6194fccd4658b503eb379433ba66adea60
paolino <paolo.veronelli@gmail.com>
2026-01-19 10:10:01 +0000

    freat: add rocksdb application

---
+++ src/Cardano/N2N/Client/Application/Run/Application.hs
@@ -0,0 +1,191 @@
+{-# LANGUAGE NumericUnderscores #-}
+
+module Cardano.N2N.Client.Application.Run.Application
+    ( application
+    )
+where
+
+import CSMT ()
+import Cardano.N2N.Client.Application.BlockFetch
+    ( mkBlockFetchApplication
+    )
+import Cardano.N2N.Client.Application.ChainSync
+    ( mkChainSyncApplication
+    )
+import Cardano.N2N.Client.Application.Database.Interface
+    ( Operation (..)
+    , State (..)
+    , Update (..)
+    )
+import Cardano.N2N.Client.Application.Metrics
+    ( MetricsEvent (..)
+    , MetricsParams (..)
+    , metricsTracer
+    )
+import Cardano.N2N.Client.Application.Options
+    ( Options (..)
+    )
+import Cardano.N2N.Client.Application.Run.RenderMetrics
+    ( renderMetrics
+    )
+import Cardano.N2N.Client.Application.UTxOs (Change (..), uTxOs)
+import Cardano.N2N.Client.Ouroboros.Connection (runNodeApplication)
+import Cardano.N2N.Client.Ouroboros.Types
+    ( Block
+    , Follower (..)
+    , Intersector (..)
+    , Point
+    , ProgressOrRewind (..)
+    )
+import Control.Exception (throwIO)
+import Control.Monad (replicateM_)
+import Control.Tracer (Contravariant (contramap), traceWith)
+import Data.ByteString.Lazy (ByteString)
+import Data.Function (fix)
+import Data.IORef (IORef, newIORef, readIORef, writeIORef)
+import Ouroboros.Network.Block qualified as Network
+import Ouroboros.Network.Point
+    ( WithOrigin (..)
+    )
+import System.IO (BufferMode (..), hSetBuffering, stdout)
+
+type Updater = IORef DB
+
+origin :: Network.Point block
+origin = Network.Point{getPoint = Origin}
+
+type DB =
+    State
+        IO
+        Point
+        ByteString
+        ByteString
+
+intersector
+    :: IO () -> IO (Maybe Point) -> Updater -> Intersector (Point, Block)
+intersector trUTxO mFinality updater =
+    Intersector
+        { intersectFound = \point -> do
+            putStrLn $ "Intersected at point: " ++ show point
+            pure $ follower trUTxO mFinality updater
+        , intersectNotFound = do
+            putStrLn "Intersect not found, resetting to origin"
+            pure
+                ( intersector trUTxO mFinality updater
+                , [origin]
+                )
+        }
+
+changeToOperation :: Change -> Operation ByteString ByteString
+changeToOperation (Spend k) = Delete k
+changeToOperation (Create k v) = Insert k v
+
+follower
+    :: IO () -> IO (Maybe Point) -> Updater -> Follower (Point, Block)
+follower trUTxO mFinality updater = fix $ \go ->
+    Follower
+        { rollForward = \(point, block) -> do
+            let ops = changeToOperation <$> uTxOs block
+            -- putStrLn
+            --     $ "Rolling forward to point: "
+            --         ++ show point
+            --         ++ " with "
+            --         ++ show (length ops)
+            --         ++ " UTxO changes"
+            replicateM_ (length ops) trUTxO
+            () <- onDb updater $ \case
+                Syncing update -> do
+                    u1 <- forwardTipApply update point ops
+                    finality <- mFinality
+                    u2 <- case finality of
+                        Nothing -> pure u1
+                        Just slot -> forwardFinalityApply u1 slot
+                    pure ((), Syncing u2)
+                _ -> error "follower: cannot roll forward while intersecting"
+            pure go
+        , rollBackward = \point -> rollingBack trUTxO mFinality updater point go
+        }
+
+rollingBack
+    :: IO ()
+    -> IO (Maybe Point)
+    -> Updater
+    -> Point
+    -> Follower (Point, Block)
+    -> IO (ProgressOrRewind (Point, Block))
+rollingBack trUTxO mFinality updater point follower' = do
+    putStrLn $ "Rolling back to point: " ++ show point
+    onDb updater $ \case
+        Syncing update -> do
+            result <- rollbackTipApply update (At point)
+            case result of
+                Syncing{} -> pure (Progress follower', result)
+                Truncating u ->
+                    pure
+                        (Reset (intersector trUTxO mFinality updater), Syncing u)
+                Intersecting ps u -> do
+                    pure (Rewind ps (intersector trUTxO mFinality updater), Syncing u)
+        _ -> error "rollingBack: cannot roll back while intersecting"
+
+onDb
+    :: Updater
+    -> (DB -> IO (a, DB))
+    -> IO a
+onDb updaterRef f = do
+    updater <- readIORef updaterRef
+    (a, updater') <- f updater
+    writeIORef updaterRef updater'
+    pure a
+
+application
+    :: Options
+    -> Update IO Point ByteString ByteString
+    -> [Point]
+    -> IO (Maybe Point)
+    -> IO ()
+application
+    Options
+        { networkMagic
+        , nodeName
+        , portNumber
+        , startingPoint
+        , headersQueueSize
+        }
+    update
+    slots
+    mFinality =
+        do
+            hSetBuffering stdout NoBuffering
+            tracer <-
+                metricsTracer
+                    $ MetricsParams
+                        { qlWindow = 100
+                        , utxoSpeedWindow = 1000
+                        , blockSpeedWindow = 100
+                        , metricsOutput = renderMetrics
+                        , metricsFrequency = 1_000_000
+                        }
+            let counting = traceWith tracer UTxOChangeEvent
+
+            updater <- newIORef $ Syncing update
+            (blockFetchApplication, headerIntersector) <-
+                mkBlockFetchApplication
+                    headersQueueSize
+                    (contramap BlockFetchEvent tracer)
+                    $ intersector counting mFinality updater
+            let chainFollowingApplication =
+                    mkChainSyncApplication
+                        (contramap BlockInfoEvent tracer)
+                        headerIntersector
+                        $ if null slots then [startingPoint] else slots
+            result <-
+                runNodeApplication
+                    networkMagic
+                    nodeName
+                    portNumber
+                    chainFollowingApplication
+                    blockFetchApplication
+
+            case result of
+                Left err -> throwIO err
+                Right _ -> pure ()
---
+++ src/Cardano/N2N/Client/Application/Run/RenderMetrics.hs
@@ -0,0 +1,59 @@
+module Cardano.N2N.Client.Application.Run.RenderMetrics
+    ( renderMetrics
+    )
+where
+
+import CSMT ()
+import Cardano.N2N.Client.Application.Metrics
+    ( Metrics (..)
+    )
+import Data.Maybe (fromMaybe)
+import Ouroboros.Network.Block (SlotNo (..), blockNo, blockPoint)
+import Ouroboros.Network.Block qualified as Network
+import Ouroboros.Network.Point
+    ( WithOrigin (..)
+    , blockPointHash
+    , blockPointSlot
+    )
+import System.Console.ANSI (hClearScreen, hSetCursorPosition)
+import System.IO (stdout)
+
+renderMetrics :: Metrics -> IO ()
+renderMetrics
+    Metrics
+        { averageQueueLength
+        , maxQueueLength
+        , utxoChangesCount
+        , lastBlockPoint
+        , utxoSpeed
+        , blockSpeed
+        , currentEra
+        } = do
+        hClearScreen stdout
+        hSetCursorPosition stdout 0 0
+        putStrLn
+            $ "Average Queue Length: "
+                ++ show averageQueueLength
+                ++ "\nMax Queue Length: "
+                ++ show maxQueueLength
+                ++ "\nTotal utxo changes processed: "
+                ++ show utxoChangesCount
+                ++ "\nUTXO Change Speed (utxo changes/sec): "
+                ++ show utxoSpeed
+                ++ "\nBlock Processing Speed (blocks/sec): "
+                ++ show blockSpeed
+                ++ "\nLast Block Point: "
+                ++ maybe "N/A" renderBlockPoint lastBlockPoint
+                ++ "\nLast Block Number: "
+                ++ maybe "N/A" (show . blockNo . snd) lastBlockPoint
+                ++ "\nLast Received Block Time: "
+                ++ maybe "N/A" (show . fst) lastBlockPoint
+                ++ "\nCurrent Era: "
+                ++ fromMaybe "N/A" currentEra
+      where
+        renderBlockPoint (_, header) = case blockPoint header of
+            Network.Point Origin -> "Origin"
+            Network.Point (At block) ->
+                show (blockPointHash block)
+                    ++ "@"
+                    ++ show (unSlotNo $ blockPointSlot block)
